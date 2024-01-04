1cb7f34248ac3a780a1db6c4ce7b24f260288359
Vinicius Santos <vinicius.santanasantos@yahoo.com.br>
2024-01-04 06:40:36 -0300

    feat: create rit folder or see if it already exists

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let args = std::env::args().collect::<Vec<String>>();
+
+    let command = args.get(1).expect("No command provided");
+
+    if command == "init" {
+        let test = std::fs::metadata(".rit");
+        if test.is_ok() {
+            println!("Already a rit repository");
+            return;
+        }
+
+        std::fs::create_dir(".rit").expect("Failed to create .rit directory");
+        std::fs::create_dir(".rit/objects").expect("Failed to create .rit/objects directory");
+        std::fs::create_dir(".rit/refs").expect("Failed to create .rit/refs directory");
+    }
 }
